987854b67d9a197804edc7cd9ecd12d0b1cb9f1a
Prasanna-0411 <prasanna.officialuser@gmail.com>
2024-09-08 21:56:03 +0530

    Implemented Employee Models

---
+++ Model/Models/EmployeeModels.cs
@@ -0,0 +1,15 @@
+﻿namespace Model
+{
+    public class EmployeeModels
+    {
+        public int EmployeeId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public string? ModifiedBy { get; set; }
+        public int RoleId { get; set; }
+    }
+}
