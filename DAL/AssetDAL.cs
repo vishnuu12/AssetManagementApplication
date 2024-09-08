8e60d59d533401e0d28862b764c1332249e510ad
btsvinith <vinith.sivam@gmail.com>
2024-09-08 21:32:12 +0530

    Modified GetAssetById method in Asset modules

--- DAL/AssetDAL.cs
+++ DAL/AssetDAL.cs
@@ -9,10 +9,10 @@ namespace DAL
         {
             List<AssetModels> asset = new List<AssetModels>();
             AssetModels asset1 = new AssetModels() {Id = 1,Name="Vinith",Description="HR",AssetType="aaa",CreatedTime= DateTime.Now,CreatedBy="Dhoni",ModifiedTime=DateTime.Now,ModifiedBy="Nayak"};
-            AssetModels asset2 = new AssetModels() {Id = 1,Name="ss",Description="HR",AssetType="aaa",CreatedTime= DateTime.Now,CreatedBy="Dhoni",ModifiedTime=DateTime.Now,ModifiedBy="Nayak"};
+            AssetModels asset2 = new AssetModels() {Id = 2,Name="ss",Description="HR",AssetType="aaa",CreatedTime= DateTime.Now,CreatedBy="Dhoni",ModifiedTime=DateTime.Now,ModifiedBy="Nayak"};
             asset.Add(asset1);
             asset.Add(asset2);
-            return asset;
+            return asset.Where(x => x.Id == id).ToList();
         }
 
     }
